6d5a68dcf4cc29c40b9bd48de5a560a8a0784dbd
amckean12 <amckean@me.com>
2019-06-03 15:10:01 -0500

    Init redux stores

---
+++ src/reducers/root-reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import routeReducer from './route-reducer'
+
+const rootReducer = combineReducers({
+  routeReducer,
+})
+
+export default rootReducer
---
+++ src/reducers/route-reducer.js
@@ -0,0 +1,9 @@
+const initialState= {
+  route: "/"
+}
+ export default function reducer(state = initialState, action){
+   switch(action.type){
+     default:
+      return state;
+   }
+ }
