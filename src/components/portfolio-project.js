b37701be85b83f3ad3557b9b60590126516a7dbc
amckean12 <amckean@me.com>
2019-06-04 12:38:45 -0500

    Finish portfolio site layout

--- src/components/portfolio-project.js
+++ src/components/portfolio-project.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import '../portfolio.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const ProjectComponent = (props) => (
   <div className="project-component">
-    <div className="project-image">
+    <div className={`project-image`}>
       Hi
     </div>
-    <div className="project-info">
-      <h1>Piatto</h1>
+    <div className={`project-info`}>
+      <h2>{props.title}</h2>
+      <p>Tech Used:<span>{props.techUsed}</span></p>
+      <p>About: <span>{props.about}</span></p>
+      <div className="key-features">
+        <p>Key Dev. Features</p>
+        <ul>
+          { props.keyfeatures.map((feature) =>
+            <li className="feature-item">{feature}</li>) }
+        </ul>
+      </div>
+      <div className="project-link-container">
+        <a href={props.link}><FontAwesomeIcon icon={faGithub}/></a>
+      </div>
     </div>
   </div>
 );
--- src/containers/portfolio-container.js
+++ src/containers/portfolio-container.js
@@ -10,8 +10,20 @@ class PortfolioContainer extends Component {
    render(){
      return(
        <div className="Portfolio-Container">
-        <ProjectComponent />
-        <ProjectComponent />
+        <ProjectComponent
+          title="Piatto"
+          about="A dynamic cookbook that allows users to build menus, based on searching for recipes by tags."
+          techUsed="Javascript | Ruby | React | Redux | Rails | Knock | HTML | CSS | PostgreSQL | Heroku"
+          keyfeatures={["JWT authentication for API access", "Recipe tag lookup/creation algorithm", "User Persistance with Redux-Persist", "Asynchronous web requests with Redux-Thunk"]}
+          link = "https://github.com/amckean12/piatto-react"
+          />
+        <ProjectComponent
+          title="My Portfolio Site"
+          about="This current site. I use this for my blog as well as displaying projects I've worked on."
+          techUsed="Javascript | React | Redux | HTML | CSS | Github Pages"
+          keyfeatures={["Minimal and Responsive Design", "React Router used for Navigation", "Responsive navigation links"]}
+          link = "https://github.com/amckean12/alex-site.github.io"
+          />
        </div>
      )
    }
