61cf49e9120e7667421506d9b8212e218039c2a5
amckean12 <amckean@me.com>
2019-06-10 12:23:00 -0500

    Set up blog links

--- src/containers/blog-container.js
+++ src/containers/blog-container.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Blog from '../components/blog-component.js'
+import '../blog.css'
 
 //Blogs
 import { whatIveLearned } from '../blogs/what-ive-learned.js'
@@ -9,7 +10,17 @@ class BlogContainer extends Component {
      return(
        <div className="blog-container">
          <h2>Dev. Blog</h2>
-         <Blog content={whatIveLearned}/>
+         <div className="blog-index">
+           <h3>Piatto Portfolio Project</h3>
+           <ul>
+             <li>What I've Learned From Piatto</li>
+             <li>Application Authentication Part 1- Server Side</li>
+             <li>Application Authentication Part 2- Client Side</li>
+             <li>Nested Attributes in Forms</li>
+             <li>Setting up Fullstack App Part 1- Rails API</li>
+             <li>Setting up Fullstack App Part 2- React Client</li>
+           </ul>
+         </div>
        </div>
      )
    }
