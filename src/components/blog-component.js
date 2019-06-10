e998acdd6f679dc1147a9060257d3891165060ad
amckean12 <amckean@me.com>
2019-06-10 13:54:27 -0500

    Began work on floating blog post

--- src/components/blog-component.js
+++ src/components/blog-component.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Blog = (props) => (
   <div className="blog-component">
-    {props.content}
+    {props.title}
   </div>
 );
 
--- src/containers/blog-container.js
+++ src/containers/blog-container.js
@@ -6,24 +6,42 @@ import '../blog.css'
 import { whatIveLearned } from '../blogs/what-ive-learned.js'
 
 class BlogContainer extends Component {
+  state = {
+    currentBlogTitle: "What I've Learned From Piatto"
+  }
+
+  renderedBlog = () => {
+    return( <Blog title={this.state.currentBlogTitle}/>)
+  }
+
    render(){
-     const renderBlogBox = () =>{
-       console.log("Hello")
-     }
      return(
        <div className="blog-container">
          <h2>Dev. Blog</h2>
          <div className="blog-index">
            <h3>Piatto Portfolio Project</h3>
            <ul>
-             <li>What I've Learned From Piatto</li>
-             <li>Application Authentication Part 1- Server Side</li>
-             <li>Application Authentication Part 2- Client Side</li>
-             <li>Nested Attributes in Forms</li>
-             <li>Setting up Fullstack App Part 1- Rails API</li>
-             <li>Setting up Fullstack App Part 2- React Client</li>
+             <li>
+               <p>What I've Learned From Piatto</p>
+             </li>
+             <li>
+               <p>Application Authentication Part 1- Server Side</p>
+             </li>
+             <li>
+               <p>Application Authentication Part 2- Client Side</p>
+             </li>
+             <li>
+               <p>Nested Attributes in Forms</p>
+             </li>
+             <li>
+               <p>Setting up Fullstack App Part 1- Rails API</p>
+             </li>
+             <li>
+               <p>Setting up Fullstack App Part 2- React Client</p>
+             </li>
            </ul>
          </div>
+         { this.renderedBlog() }
        </div>
      )
    }
