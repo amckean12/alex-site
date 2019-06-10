76c8bcd79abfc5fc362ece15b805b0d33f2553f6
amckean12 <amckean@me.com>
2019-06-10 14:23:38 -0500

    Added FA exit button to blog container

--- src/components/blog-component.js
+++ src/components/blog-component.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const Blog = (props) => (
   <div className="blog-component">
     <h3>{props.title}</h3>
-    
+    <FontAwesomeIcon icon={faTimes} />
   </div>
 );
 
