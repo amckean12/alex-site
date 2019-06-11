dde490413b0d3e8fd0fe5c96b3a1b5831069cbf8
amckean12 <amckean@me.com>
2019-06-11 14:00:44 -0500

    removed pic need to find another way

--- src/components/project-image-component.js
+++ src/components/project-image-component.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import portfolioImg from '../images/Portfolio-Site-Landing.png'
 
 const ProjectImageComponent = (props) => (
   <div className="project-image-slideshow">
-    <img src={portfolioImg} alt="portfolio-site" />
   </div>
 );
 
