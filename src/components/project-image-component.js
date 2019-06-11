978f1623359f37429bce52ae6e575f8809acb6a5
amckean12 <amckean@me.com>
2019-06-11 13:59:31 -0500

    Added picture to component

--- src/components/project-image-component.js
+++ src/components/project-image-component.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import portfolioImg from '../images/Portfolio-Site-Landing.png'
 
 const ProjectImageComponent = (props) => (
   <div className="project-image-slideshow">
-    
+    <img src={portfolioImg} alt="portfolio-site" />
   </div>
 );
 
