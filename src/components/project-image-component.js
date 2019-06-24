530de6bec92fdf66a17c88c22aa21509789fee22
amckean12 <amckean@me.com>
2019-06-24 14:38:51 -0500

    Added port images

--- src/components/project-image-component.js
+++ src/components/project-image-component.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
 const ProjectImageComponent = (props) => (
-  <div className="project-image-slideshow">
-    <button onClick={props.handleClick}>See Demo</button>
-    <br></br>
-    <button onClick={props.handleClick}>Images</button>
+  <div className="project-image-container">
+
   </div>
 );
 
